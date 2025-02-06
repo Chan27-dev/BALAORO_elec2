880821ee49187628150ef3ebd7873bf1717db0c7
Gio Kent <kennethbelardo55@gmail.com>
2025-02-06 21:54:35 +0800

    chore: Revise py code

--- lab#.py
+++ lab#.py
@@ -1,36 +1,34 @@
 
-//SHOW DATASETS
+# SHOW DATASETS
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-
-# Initialize a Spark session if not already initialized
-spark = SparkSession.builder.appName("ReadCSV").getOrCreate()
-
-# Assuming that your csv is in the current google colab working directory where your notebook is stored
 import os
+
+spark = SparkSession.builder.appName("Cereal").getOrCreate()
+
 data_path = os.path.join(os.getcwd(), "cereal.csv") 
 print(f"Reading CSV from: {data_path}")
 
-# Read the CSV into a Spark DataFrame
 df = spark.read.csv(data_path, header=True, inferSchema=True)
 
 # Display the DataFrame to verify
 df.show()
 
-//RANGE PARTIONING
-from pyspark.sql.functions import col
+# PARTITIONING STRATEGY #1: RANGE PARTIONING
 
 range_partitioned_df = df.orderBy(col("rating")).repartitionByRange(4, col("rating"))
 print(f"Number of partitions (Range): {range_partitioned_df.rdd.getNumPartitions()}")
 
-# Collect and print data in each partition
 for i in range(range_partitioned_df.rdd.getNumPartitions()):
     print(f"Partition {i}:")
     partition_data = range_partitioned_df.rdd.mapPartitionsWithIndex(lambda index, it: it if index == i else []).collect()
     for row in partition_data:
-        print(row)  # Print each row as a Row object
+        print(row) 
+        
+# TRANSFORMATION PIPELINE
+range_partitioned_df.groupBy("mfr").avg("rating").show()
 
-//CUSTOM PARTIONING
+# PARTITIONING STRATEGY #2: CUSTOM PARTIONING
 df = df.withColumn(
     "sugar_category",
     when(df["sugars"] < 5, "Low Sugar")
@@ -43,13 +41,8 @@ sugar_partitioned_df = df.repartition(3, "sugar_category")
 print(f"Number of partitions (Sugar Partitioning): {sugar_partitioned_df.rdd.getNumPartitions()}")
 sugar_partitioned_df.groupBy("sugar_category").count().show()
 
-////TRANSFORMATION PIPELINE
+# TRANSFORMATION PIPELINE
 sugar_partitioned_df.groupBy("sugar_category").max("calories").show()
-
-//TRANSFORMATION PIPELINE
 sugar_partitioned_df.groupBy("sugar_category").avg("rating").show()
-
-//TRANSFORMATION PIPELINE
-range_partitioned_df.groupBy("mfr").avg("rating").show()
 
 
