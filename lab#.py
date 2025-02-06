0b6e68ee88aa088abc8f10702c664fcdd95026c4
Gio Kent <kennethbelardo55@gmail.com>
2025-02-06 22:29:25 +0800

    refactor: Optimize

--- lab#.py
+++ lab#.py
@@ -16,19 +16,22 @@ df.show()
 
 # PARTITIONING STRATEGY #1: RANGE PARTIONING
 
+print("Partition 1: Range Partitioning")
 range_partitioned_df = df.orderBy(col("rating")).repartitionByRange(4, col("rating"))
 print(f"Number of partitions (Range): {range_partitioned_df.rdd.getNumPartitions()}")
 
 for i in range(range_partitioned_df.rdd.getNumPartitions()):
     print(f"Partition {i}:")
     partition_data = range_partitioned_df.rdd.mapPartitionsWithIndex(lambda index, it: it if index == i else []).collect()
-    for row in partition_data:
-        print(row) 
-        
-# TRANSFORMATION PIPELINE
-range_partitioned_df.groupBy("mfr").avg("rating").show()
+    # TRANSFORMATION PIPELINE
+    partition_data.groupBy("mfr").avg("rating").show()
 
+    # for row in partition_data:
+    #     print(row) 
+        
 # PARTITIONING STRATEGY #2: CUSTOM PARTIONING
+
+print("Partition 2: Custom Partitioning")
 df = df.withColumn(
     "sugar_category",
     when(df["sugars"] < 5, "Low Sugar")
@@ -39,10 +42,14 @@ df = df.withColumn(
 sugar_partitioned_df = df.repartition(3, "sugar_category")
 
 print(f"Number of partitions (Sugar Partitioning): {sugar_partitioned_df.rdd.getNumPartitions()}")
-sugar_partitioned_df.groupBy("sugar_category").count().show()
+for i in range(sugar_partitioned_df.rdd.getNumPartitions()):
+    print(f"Partition {i}:")
+    partition_data = sugar_partitioned_df.rdd.mapPartitionsWithIndex(lambda index, it: it if index == i else []).collect()
+    # TRANSFORMATION PIPELINE
+    partition_data.groupBy("sugar_category").max("calories").show()
+    partition_data.groupBy("sugar_category").avg("rating").show()
 
-# TRANSFORMATION PIPELINE
-sugar_partitioned_df.groupBy("sugar_category").max("calories").show()
-sugar_partitioned_df.groupBy("sugar_category").avg("rating").show()
+
+
 
 
